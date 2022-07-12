ff4074128fcd363892b3e4ea3950de9cb5a9ef1b
shumakooov <shumakov.13@yandex.ru>
2022-07-12 16:09:57 +0500

    first commit

---
+++ main2.py
@@ -0,0 +1,47 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Price(BaseModel):
+    name: str
+    price: float
+
+
+PRICES_DB = [
+    Price(name="test1", price=1000),
+    Price(name="test2", price=2000),
+    Price(name="test3", price=100),
+    Price(name="test4", price=999),
+]
+
+
+@app.get("/prices")
+def read_prices():
+    return PRICES_DB
+
+
+@app.get("/price/{item_id}")
+def read_price(item_id: int):
+    return PRICES_DB[item_id]
+
+
+@app.post("/price/create")
+def create_price(item: Price):
+    PRICES_DB.append(item)
+    return {"status": "ok"}
+
+
+@app.put("/price/{item_id}")
+def update_price(item_id: int, item: Price):
+    PRICES_DB[item_id] = item
+    return {"status": "ok"}
+
+
+@app.delete("/price/{item_id}")
+def update_price(item_id: int):
+    del PRICES_DB[item_id]
+    return {"status": "ok"}
---
+++ prices_goldapple.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup
+from re import sub
+from sqlalchemy import create_engine, Column, Integer, String, Numeric, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+from datetime import datetime
+from selenium import webdriver
+import time
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+PRODUCT_URL = "https://goldapple.ru/makijazh"
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:70.0) Gecko/20100101 Firefox/70.0"
+}
+driver = webdriver.Chrome(executable_path='./chromedriver.exe')
+#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get(PRODUCT_URL)
+
+driver.set_window_size(1920, 1080)
+time.sleep(1)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(3)
+
+html = driver.page_source
+soup = BeautifulSoup(html, "lxml")
+categories = soup.find_all("div", class_="product-item-category-title")
+brand_names = soup.find_all("span", class_="catalog-brand-name-span")
+product_names = soup.find_all("span", class_="catalog-product-name-span")
+prices = soup.find_all("span", class_="price")
+
+driver.close()
+driver.quit()
+
+Base = declarative_base()
+
+
+class Price(Base):
+    __tablename__ = "price"
+    id = Column(Integer, primary_key=True)
+    category = Column(String)
+    brand_name = Column(String(64))
+    product_name = Column(String(64))
+    price = Column(String(16))
+    price_int = Column(Numeric(10, 2))
+    update_date = Column(DateTime)
+
+
+engine = create_engine("sqlite:///db_goldapple.sqlite")
+Base.metadata.create_all(engine)
+
+session = Session(bind=engine)
+
+
+def add_product(category, brand_name, product_name, price, price_int):
+    is_exist = session.query(Price).filter(Price.product_name == product_name).all()
+    if not is_exist:
+        session.add(
+            Price(
+                category=category,
+                brand_name=brand_name,
+                product_name=product_name,
+                price=price,
+                price_int=price_int,
+                update_date=datetime.now()
+            )
+        )
+        session.commit()
+
+
+for item in range(0, len(product_names)):
+    add_product(categories[item].get_text().strip(),
+                brand_names[item].get_text(),
+                product_names[item].get_text(),
+                prices[item].get_text(),
+                int(sub("[^0-9]", "", prices[item].get_text()))
+                )
